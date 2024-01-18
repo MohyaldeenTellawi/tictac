39e42f018d3ec78e819813dbdbf7bfdad8739445
Mohyaldeen <mohyaldeentellawi@.com>
2024-01-18 23:57:29 +0300

    updateUI

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:youtub7/board_tile.dart';
 import 'package:youtub7/tile_state.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
 
@@ -14,9 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'TIC TAC',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(useMaterial3: true),
       home: const MyHomePage(),
     );
   }
@@ -25,15 +27,11 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
-
-
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   Orientation orientation = Orientation.portrait;
   final navigatorKey = GlobalKey<NavigatorState>();
   var _boardState = List.filled(9, TileState.EMPTY);
@@ -41,109 +39,93 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return
-    Scaffold(
+    return Scaffold(
       backgroundColor: const Color.fromRGBO(245, 222, 179, 1),
-      appBar: AppBar(
-        elevation: 0,
+      appBar: CupertinoNavigationBar(
+        leading: IconButton(
+            onPressed: _returnGame,
+            icon: const Icon(CupertinoIcons.refresh_thin)),
         backgroundColor: const Color.fromRGBO(245, 222, 179, 1),
-        title: const Text('TIC TAC Game',
-        style: TextStyle(color: Color.fromRGBO(149, 69, 53, 1),
-        fontSize: 25,fontWeight: FontWeight.w800),
+        middle: const Text(
+          'TIC TAC',
+          style: TextStyle(
+              color: Color.fromRGBO(149, 69, 53, 1),
+              fontSize: 25,
+              fontWeight: FontWeight.w800),
         ),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Center(
-            child: Stack(children:[
+            child: Stack(clipBehavior: Clip.none, children: [
               Image.asset('images/board.png'),
               _boardTiles(),
             ]),
           ),
+          const SizedBox(height: 20),
+          const Text.rich(
+              TextSpan(text: 'Developed By Flutter 2022, Updated in 2023')),
         ],
-      ),
-      bottomNavigationBar:  Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 120,
-            vertical: 50
-        ),
-        child: OutlinedButton(
-          style: ButtonStyle(
-              overlayColor: MaterialStateProperty.all(
-                const Color.fromRGBO(210, 125, 45, 1),
-              ),
-              alignment: Alignment.center,
-              shape: MaterialStateProperty.all(
-                  RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50)))),
-          onPressed: _returnGame,
-          child: const Text('new again',
-            style: TextStyle(
-              fontSize: 30,
-              color: Color.fromRGBO(149, 69, 53, 1),
-            ),
-          ),
-        ),
       ),
     );
   }
 
-  Widget _boardTiles () {
-    return Builder(builder: (context){
-    final boardDimension = MediaQuery.of(context).size.width;
-    final tileDimension = boardDimension / 3;
+  Widget _boardTiles() {
+    return Builder(builder: (context) {
+      final boardDimension = MediaQuery.of(context).size.width;
+      final tileDimension = boardDimension / 3;
 
-    return SizedBox(
-      width: boardDimension,
-      height: boardDimension,
-      child: Column(
-        children: 
-        chunk(_boardState, 3).asMap().entries.map((entry) {
-          final chunkIndex = entry.key;
-          final tileStateChunk = entry.value;
+      return SizedBox(
+        width: boardDimension,
+        height: boardDimension,
+        child: Column(
+          children: chunk(_boardState, 3).asMap().entries.map((entry) {
+            final chunkIndex = entry.key;
+            final tileStateChunk = entry.value;
 
-          return Row(
-            children: tileStateChunk.asMap().entries.map((innerEntry)
-          {
-            final innerIndex = innerEntry.key;
-            final tileState = innerEntry.value;
-            final tileIndex = (chunkIndex*3)+innerIndex;
+            return Row(
+              children: tileStateChunk.asMap().entries.map((innerEntry) {
+                final innerIndex = innerEntry.key;
+                final tileState = innerEntry.value;
+                final tileIndex = (chunkIndex * 3) + innerIndex;
 
-            return BoardTile(
-                dimension: tileDimension,
-                onPressed: ()=>_updateTileStateForIndex(tileIndex),
-                tileState: tileState);
-            }).toList(),
-          );
-        }).toList(),
-      ),
-    );
+                return BoardTile(
+                    dimension: tileDimension,
+                    onPressed: () => _updateTileStateForIndex(tileIndex),
+                    tileState: tileState);
+              }).toList(),
+            );
+          }).toList(),
+        ),
+      );
     });
   }
 
-  void _updateTileStateForIndex(int selectedIndex){
-    if(_boardState[selectedIndex]==TileState.EMPTY){
+  void _updateTileStateForIndex(int selectedIndex) {
+    if (_boardState[selectedIndex] == TileState.EMPTY) {
       setState(() {
-        _boardState[selectedIndex]=_currentTurn;
-        _currentTurn = _currentTurn==TileState.CROSS?TileState.CIRCLE:
-            TileState.CROSS;
+        _boardState[selectedIndex] = _currentTurn;
+        _currentTurn = _currentTurn == TileState.CROSS
+            ? TileState.CIRCLE
+            : TileState.CROSS;
       });
       final winner = _findWinner();
-      if(winner!=null){
+      if (winner != null) {
         _showWinnerDialog(winner);
       }
     }
   }
 
-
-  TileState? _findWinner (){
-    winnerForMatch(a,b,c){
-      if(_boardState[a]!=TileState.EMPTY){
-        if((_boardState[a]==_boardState[b])&& (_boardState[b]==_boardState[c])){
+  TileState? _findWinner() {
+    winnerForMatch(a, b, c) {
+      if (_boardState[a] != TileState.EMPTY) {
+        if ((_boardState[a] == _boardState[b]) &&
+            (_boardState[b] == _boardState[c])) {
           return _boardState[a];
         }
       }
-     return null;
+      return null;
     }
 
     final checks = [
@@ -157,8 +139,8 @@ class _MyHomePageState extends State<MyHomePage> {
       winnerForMatch(2, 4, 6),
     ];
     TileState? winner;
-    for(int i=0;i<checks.length;i++){
-      if(checks[i]!=null){
+    for (int i = 0; i < checks.length; i++) {
+      if (checks[i] != null) {
         winner = checks[i];
         break;
       }
@@ -166,44 +148,47 @@ class _MyHomePageState extends State<MyHomePage> {
     return winner;
   }
 
-
-  void _showWinnerDialog(TileState tileState){
-    showDialog(
-      barrierDismissible: false,
+  void _showWinnerDialog(TileState tileState) {
+    showAdaptiveDialog(
+        barrierDismissible: false,
         context: context,
-        builder: (_){
-      return AlertDialog(
-        elevation: 40,
-        backgroundColor:const Color.fromRGBO(245, 222, 179, 1),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(50)),
-        shadowColor: const Color.fromRGBO(149, 69, 53, 1),
-        title: const Center(child: Text('Winner',
-        style: TextStyle(fontWeight: FontWeight.bold,
-        fontSize: 50,color: Color.fromRGBO(149, 69, 53, 1),
-        ),
-        )),
-        content:
-        Image.asset(tileState == TileState.CROSS?'images/x.png':'images/o.png'),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 20),
-            child: TextButton(
-                onPressed: _resetGame,
-                child: const Text('New Game',
-                style: TextStyle(
-                  color: Color.fromRGBO(149, 69, 53, 1),
-                  fontSize: 20,fontWeight: FontWeight.w700
-                ),
-                )),
-          )
-        ],
-      );
+        builder: (_) {
+          return AlertDialog.adaptive(
+            elevation: 40,
+            backgroundColor: const Color.fromRGBO(245, 222, 179, 1),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+            shadowColor: const Color.fromRGBO(149, 69, 53, 1),
+            title: const Center(
+                child: Text(
+              'Winner',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 50,
+                color: Color.fromRGBO(149, 69, 53, 1),
+              ),
+            )),
+            content: Image.asset(
+                tileState == TileState.CROSS ? 'images/x.png' : 'images/o.png'),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 20),
+                child: TextButton(
+                    onPressed: _resetGame,
+                    child: const Text(
+                      'New Game',
+                      style: TextStyle(
+                          color: Color.fromRGBO(149, 69, 53, 1),
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700),
+                    )),
+              )
+            ],
+          );
         });
   }
 
-
-  void _resetGame (){
+  void _resetGame() {
     setState(() {
       _boardState = List.filled(9, TileState.EMPTY);
       _currentTurn = TileState.CROSS;
@@ -211,7 +196,7 @@ class _MyHomePageState extends State<MyHomePage> {
     Navigator.of(context).pop();
   }
 
-  void _returnGame(){
+  void _returnGame() {
     setState(() {
       _boardState = List.filled(9, TileState.EMPTY);
       _currentTurn = TileState.CROSS;
